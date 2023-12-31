8ff497cbe61ae4d3e531cd82cc2c1c0948cac3fe
KateLu666 <KateLuDev@outlook.com>
2023-12-31 04:42:22 -0500

    Update navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -26,8 +26,8 @@ const Navbar = () => {
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-        <Link href="/" className="my-2">
-          <Image src="/images/Logo.png" alt="Logo" width={200} height={100} />
+        <Link href="/" className="text-white font-semibold text-3xl">
+          K.L
         </Link>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
